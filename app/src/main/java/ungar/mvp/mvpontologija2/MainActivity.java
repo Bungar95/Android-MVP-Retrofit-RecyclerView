c428e4417aa1850df5d116e4d110463a77f76c49
Borna Ungar <36451381+Bungar95@users.noreply.github.com>
2020-11-16 23:15:05 +0100

    changed click listener code a bit

--- app/src/main/java/ungar/mvp/mvpontologija2/MainActivity.java
+++ app/src/main/java/ungar/mvp/mvpontologija2/MainActivity.java
@@ -1,22 +1,18 @@
 package ungar.mvp.mvpontologija2;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.Toast;
 
 import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import butterknife.BindView;
+
+
 import butterknife.ButterKnife;
 import ungar.mvp.mvpontologija2.adapter.AdapterListe;
 import ungar.mvp.mvpontologija2.model.Ontologija;
@@ -24,12 +20,11 @@ import ungar.mvp.mvpontologija2.presenter.OntologijaPresenter;
 import ungar.mvp.mvpontologija2.presenter.OntologijaSucelje;
 import ungar.mvp.mvpontologija2.presenter.RecyclerViewClickListener;
 
-public class MainActivity extends AppCompatActivity implements OntologijaSucelje.View {
+public class MainActivity extends AppCompatActivity implements OntologijaSucelje.View, RecyclerViewClickListener {
 
     private OntologijaSucelje.Presenter mPresenter;
     private RecyclerView recyclerView;
     private AdapterListe adapter;
-    private List<Ontologija> ontologija;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,24 +39,22 @@ public class MainActivity extends AppCompatActivity implements OntologijaSucelje
 
     @Override
     public void init() {
-        recyclerView = (RecyclerView) findViewById(R.id.lista);
+        adapter = new AdapterListe(this);
+        adapter.setRecyclerViewClickListener(this);
+        recyclerView = findViewById(R.id.lista);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        ontologija = new ArrayList<>();
-        adapter = new AdapterListe(ontologija, new RecyclerViewClickListener() {
-            @Override
-            public void onItemClick(Ontologija ontologija) {
-                Snackbar.make(recyclerView, "Klik: "+ ontologija.getNaziv(), BaseTransientBottomBar.LENGTH_SHORT).show();
-            }
-        });
         recyclerView.setAdapter(adapter);
     }
 
     @Override
     public void onGetResult(List<Ontologija> ontologijaList) {
-
-        ontologija.addAll(ontologijaList);
+        adapter.setOntologija(ontologijaList);
         adapter.notifyDataSetChanged();
 
     }
 
+    @Override
+    public void onItemClick(Ontologija ontologija) {
+        Snackbar.make(recyclerView, "klik", BaseTransientBottomBar.LENGTH_SHORT).show();
+    }
 }
--- app/src/main/java/ungar/mvp/mvpontologija2/adapter/AdapterListe.java
+++ app/src/main/java/ungar/mvp/mvpontologija2/adapter/AdapterListe.java
@@ -1,5 +1,6 @@
 package ungar.mvp.mvpontologija2.adapter;
 
+import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,11 +8,13 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ungar.mvp.mvpontologija2.R;
@@ -24,16 +27,21 @@ import static java.security.AccessController.getContext;
 public class AdapterListe extends RecyclerView.Adapter<AdapterListe.Red>{
 
     List<Ontologija> ontologija;
+    private LayoutInflater layoutInflater;
     RecyclerViewClickListener recyclerViewClickListener;
 
-    public AdapterListe(List<Ontologija> ontologija, RecyclerViewClickListener listener) {
+    public AdapterListe(Context context){
+        layoutInflater = LayoutInflater.from(context);
+        ontologija = new ArrayList<>();
+    }
+
+    public void setOntologija(List<Ontologija> ontologija) {
         this.ontologija = ontologija;
-        this.recyclerViewClickListener = listener;
     }
 
     @Override
-    public Red onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.red_liste, parent, false);
+    public Red onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = layoutInflater.inflate(R.layout.red_liste, parent, false);
         return new Red(view);
     }
 
@@ -45,28 +53,35 @@ public class AdapterListe extends RecyclerView.Adapter<AdapterListe.Red>{
         red.opis.setText(o.getOpis());
         red.anotacija.setText(o.getAnotacija());
         //Picasso.get().load(o.getUrlSlika()).into(red.slika);
-        red.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                recyclerViewClickListener.onItemClick(o);
-            }
-        });
+
     }
     @Override
     public int getItemCount() {
         return ontologija==null ? 0 : ontologija.size();
     }
 
-    public class Red extends RecyclerView.ViewHolder {
+    public class Red extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         TextView naziv, tip, opis, anotacija;
 
-        public Red(View itemView) {
+        public Red(@NonNull View itemView) {
             super(itemView);
             naziv = itemView.findViewById(R.id.naziv);
             tip = itemView.findViewById(R.id.tip);
             opis = itemView.findViewById(R.id.opis);
             anotacija = itemView.findViewById(R.id.anotacija);
+            itemView.setOnClickListener(this);
         }
+
+        @Override
+        public void onClick(View view) {
+            if(recyclerViewClickListener!=null){
+                recyclerViewClickListener.onItemClick(ontologija.get(getAdapterPosition()));
+            }
+        }
+    }
+
+    public void setRecyclerViewClickListener(RecyclerViewClickListener recyclerViewClickListener) {
+        this.recyclerViewClickListener = recyclerViewClickListener;
     }
 }
