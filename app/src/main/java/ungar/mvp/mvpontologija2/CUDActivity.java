fc44cb6fd584e25a1e68e662c3a9ff0e60497c26
Borna Ungar <36451381+Bungar95@users.noreply.github.com>
2020-11-18 17:47:31 +0100

    creating CUD - postponed until implementing Room database

--- app/src/main/java/ungar/mvp/mvpontologija2/CUDActivity.java
+++ app/src/main/java/ungar/mvp/mvpontologija2/CUDActivity.java
@@ -3,12 +3,19 @@ package ungar.mvp.mvpontologija2;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import butterknife.BindView;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
-public class CUDActivity extends AppCompatActivity {
+import butterknife.BindView;
+import ungar.mvp.mvpontologija2.model.Ontologija;
+import ungar.mvp.mvpontologija2.presenter.CUDPresenter;
+import ungar.mvp.mvpontologija2.presenter.CUDSucelje;
+
+public class CUDActivity extends AppCompatActivity implements CUDSucelje.View {
 
     @BindView(R.id.editNaziv)
     EditText naziv;
@@ -21,9 +28,20 @@ public class CUDActivity extends AppCompatActivity {
     @BindView(R.id.btnDodaj)
     Button btnDodaj;
 
+    private CUDSucelje.Presenter mPresenter;
+    private Ontologija ontologija;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cud);
+        mPresenter = new CUDPresenter(this, ontologija);
+        mPresenter.created();
+    }
+
+    @Override
+    public void init() {
+        /* Dodati ću CUD nakon što implementiram Room bazu podataka */
+        Snackbar.make(findViewById(R.id.cudLayout),"Trenutno ne radi.", BaseTransientBottomBar.LENGTH_LONG).show();
     }
 }
